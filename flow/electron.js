jgora/tutanota

commit 33facc6a18ff0b65a65049bf0983b96b232236c7

[desktop] electron stopped firing found-in-page if request has findNext=false, always set it to true (#2393)
also adds a little more bookkeeping to the findInPage request handling and reject
old request when a new one comes in.

doesn't require extra changes to admin client since the changed files are used by it as-is and the protocol didn't change.

fix #2355

src/desktop/ApplicationWindow.js
@@ -13,6 +13,7 @@ import {Keys} from "../api/common/TutanotaConstants"
 import type {Shortcut} from "../misc/KeyManager"
 import {DesktopConfig} from "./config/DesktopConfig"
 import path from "path"
+import {noOp} from "../api/common/utils/Utils"
 
 const MINIMUM_WINDOW_SIZE: number = 350
 
@@ -31,6 +32,7 @@ export class ApplicationWindow {
 	_findingInPage: boolean = false;
 	_skipNextSearchBarBlur: boolean = false;
 	_lastSearchRequest: ?[string, {forward: boolean, matchCase: boolean}] = null;
+	_lastSearchPromiseReject: () => void;
 	_shortcuts: Array<Shortcut>;
 	id: number;
 
@@ -38,6 +40,7 @@ export class ApplicationWindow {
 		this._userInfo = null
 		this._ipc = wm.ipc
 		this._startFile = DesktopUtils.pathToFileURL(path.join(app.getAppPath(), conf.getConst("desktophtml")),)
+		this._lastSearchPromiseReject = noOp
 
 		const isMac = process.platform === 'darwin';
 		this._shortcuts = [
@@ -294,10 +297,16 @@ export class ApplicationWindow {
 		if (searchTerm !== '') {
 			this._lastSearchRequest = [searchTerm, options]
 			this._browserWindow.webContents.findInPage(searchTerm, options)
-			return new Promise((resolve) => {
-				this._browserWindow.webContents.once('found-in-page', (ev: Event, res: FindInPageResult) => {
-					resolve(res)
-				})
+			return new Promise((resolve, reject) => {
+				this._lastSearchPromiseReject("outdated request")
+				this._lastSearchPromiseReject = reject
+				this._browserWindow.webContents
+					// the last listener might not have fired yet
+					.removeAllListeners('found-in-page')
+					.once('found-in-page', (ev: Event, res: FindInPageResult) => {
+						this._lastSearchPromiseReject = noOp
+						resolve(res)
+					})
 			})
 		} else {
 			this.stopFindInPage()

src/desktop/IPC.js
@@ -87,7 +87,10 @@ export class IPC {
 				return this.initialized(windowId).then(() => {
 					const w = this._wm.get(windowId)
 					if (w) {
+						// findInPage might reject if requests come too quickly
+						// if it's rejecting for another reason we'll have logs
 						return w.findInPage(args)
+						        .catch(e => console.log("findInPage reject:", args, e))
 					} else {
 						return {numberOfMatches: 0, currentMatch: 0}
 					}

src/gui/base/SearchInPageOverlay.js
@@ -89,7 +89,7 @@ export class SearchInPageOverlay {
 					}
 				},
 				onfocus: e => nativeApp.invokeNative(new Request("setSearchOverlayState", [true, false])),
-				oninput: e => this._find(true, false),
+				oninput: e => this._find(true, true),
 				style: {
 					width: px(250),
 					top: 0,
@@ -102,7 +102,6 @@ export class SearchInPageOverlay {
 	}
 
 	_find: ((forward: boolean, findNext: boolean) => Promise<void>) = (forward, findNext) => {
-		console.log("finding next", this._domInput.value)
 		this._skipNextBlur = true
 		return nativeApp.invokeNative(new Request("findInPage", [
 			this._domInput.value, {
@@ -173,6 +172,7 @@ export class SearchInPageOverlay {
 									if (keyCode === Keys.ESC.code) {
 										this.close()
 									}
+									// prevent key from getting picked up by shortcuts etc.
 									e.stopPropagation()
 									return true
 								},

test/client/desktop/IPCTest.js
@@ -136,11 +136,9 @@ o.spec("IPC tests", () => {
 			id: 42,
 			sendMessageToWebContents: () => {
 			},
-			findInPage: () => {
-				return {
-					then: cb => cb({numberOfMatches: 37, currentMatch: 13})
-				}
-			},
+			findInPage: () => new Promise((resolve, reject) => {
+				resolve({numberOfMatches: 37, currentMatch: 13})
+			}),
 			stopFindInPage: () => {
 
 			},
@@ -283,7 +281,7 @@ o.spec("IPC tests", () => {
 			o(windowMock.sendMessageToWebContents.args[1]).deepEquals({
 				id: 'id2',
 				type: 'response',
-				value: {numberOfMatches: 37, currentMatch: 13}// there it is
+				value: {numberOfMatches: 37, currentMatch: 13}
 			})
 
 			electronMock.ipcMain.callbacks[CALLBACK_ID]({}, JSON.stringify({

commit 06efa9d3bece5d0c804f2dbc1d3abc95c50aea4b

[desktop] fix flow errors introduced by 33facc6a18ff0b65a65049bf0983b96b232236c7

flow/electron.js
@@ -477,6 +477,7 @@ declare module 'electron' {
 	declare export class WebContents {
 		on(WebContentsEvent, (Event, ...Array<any>) => void): WebContents;
 		once(WebContentsEvent, (Event, ...Array<any>) => void): WebContents;
+		removeAllListeners(WebContentsEvent): WebContents;
 		send(string, any): void;
 		session: ElectronSession;
 		getURL(): string;

src/desktop/ApplicationWindow.js
@@ -32,7 +32,7 @@ export class ApplicationWindow {
 	_findingInPage: boolean = false;
 	_skipNextSearchBarBlur: boolean = false;
 	_lastSearchRequest: ?[string, {forward: boolean, matchCase: boolean}] = null;
-	_lastSearchPromiseReject: () => void;
+	_lastSearchPromiseReject: (?string) => void;
 	_shortcuts: Array<Shortcut>;
 	id: number;
 